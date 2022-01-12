7cb00250df8d204cebd6a8030f8a774cfed8f457
Anant van den Berg <anant.vandenberg@gmail.com>
2022-01-12 19:29:35 +0100

    Added images Lambda functions

---
+++ src/handlers/images/put-image.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const AWS = require('aws-sdk')
+AWS.config.update({ region: process.env.AWS_REGION })
+const s3 = new AWS.S3()
+
+// Change this value to adjust the signed URL's expiration
+const URL_EXPIRATION_SECONDS = 300
+
+// Main Lambda entry point
+exports.handler = async (event) => {
+  return await getUploadURL(event)
+}
+
+const getUploadURL = async function(event) {
+  const randomID = parseInt(Math.random() * 10000000)
+  const Key = `${randomID}.jpg`
+
+  // Get signed URL from S3
+  const s3Params = {
+    Bucket: process.env.UploadBucket,
+    Key,
+    Expires: URL_EXPIRATION_SECONDS,
+    ContentType: 'image/jpeg',
+
+    // This ACL makes the uploaded object publicly readable. You must also uncomment
+    // the extra permission for the Lambda function in the SAM template.
+    ACL: 'public-read'
+  }
+
+  console.log('Params: ', s3Params)
+  const uploadURL = await s3.getSignedUrlPromise('putObject', s3Params)
+
+  return JSON.stringify({
+    uploadURL: uploadURL,
+    Key
+  })
+}
